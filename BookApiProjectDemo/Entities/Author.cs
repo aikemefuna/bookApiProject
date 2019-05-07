c2a820967046f797c05b4875b8686bee83efa29c
aikemefuna <aikemefuna3@gmail.com>
2019-05-07 11:25:53 +0100

    created Entity \models

---
+++ BookApiProjectDemo/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProjectDemo.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Country Country { get; set; }
+        public virtual ICollection<BookAuthor> BookAuhors { get; set; }
+    }
+
+}
